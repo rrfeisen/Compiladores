be4e5d57efcd79d61adae408e95d07090f30f58a
Roberto Ramos <56175688+rrfeisen@users.noreply.github.com>
2026-05-25 19:19:49 -0300

    Refactor expression extraction logic in Expressao.cpp

--- Lab03/src/src-gram9/Expressao.cpp
+++ Lab03/src/src-gram9/Expressao.cpp
@@ -14,87 +14,79 @@
 
 using namespace std;
 
-// Tratei Ea, Eb, Ec, E, T e F na mesma funcao.
 Expressao* Expressao::extrai_expressao(No_arv_parse* no) {
-  // Ea,Eb,Ec,E,T,F. 16) a 33)
+  if (no == NULL) return NULL;
+
   switch(no->regra) {
-  case 16: { // 16) Ea -> ! Ea
-    ExpressaoNegacao* res16 = new ExpressaoNegacao();
-    res16->expressao = extrai_expressao(no->filhos[1]);
-    return res16;
+  case 50: { // 50) unary_expr -> KW_NOT unary_expr
+    ExpressaoNegacao* res = new ExpressaoNegacao();
+    res->expressao = extrai_expressao(no->filhos[1]);
+    return res;
   }
-  case 17: { // 17) Ea -> Eb
-    return extrai_expressao(no->filhos[0]);
+  case 36: { // 36) cmp_expr -> add_expr OP_EQ add_expr
+    ExpressaoIgualdade* res = new ExpressaoIgualdade();
+    res->esquerda = extrai_expressao(no->filhos[0]);
+    res->direita = extrai_expressao(no->filhos[2]);
+    return res;
   }
-  case 18: { // 18) Eb -> Ec == Ec
-    ExpressaoIgualdade* res18 = new ExpressaoIgualdade();
-    res18->esquerda = extrai_expressao(no->filhos[0]);
-    res18->direita = extrai_expressao(no->filhos[2]);
-    return res18;
+  case 38: { // 38) cmp_expr -> add_expr OP_LT add_expr
+    ExpressaoMenor* res = new ExpressaoMenor();
+    res->esquerda = extrai_expressao(no->filhos[0]);
+    res->direita = extrai_expressao(no->filhos[2]);
+    return res;
   }
-  case 19: { // 19) Eb -> Ec
-    return extrai_expressao(no->filhos[0]);
+  case 43: { // 43) add_expr -> add_expr OP_PLUS mul_expr
+    ExpressaoSoma* res = new ExpressaoSoma();
+    res->esquerda = extrai_expressao(no->filhos[0]);
+    res->direita = extrai_expressao(no->filhos[2]);
+    return res;
   }
-  case 20: { // 20) Ec -> E < E
-    ExpressaoMenor* res20 = new ExpressaoMenor();
-    res20->esquerda = extrai_expressao(no->filhos[0]);
-    res20->direita = extrai_expressao(no->filhos[2]);
-    return res20;
+  case 44: { // 44) add_expr -> add_expr OP_MINUS mul_expr
+    ExpressaoSubtracao* res = new ExpressaoSubtracao();
+    res->esquerda = extrai_expressao(no->filhos[0]);
+    res->direita = extrai_expressao(no->filhos[2]);
+    return res;
   }
-  case 21: { // 21) Ec -> E
-    return extrai_expressao(no->filhos[0]);
+  case 46: { // 46) mul_expr -> mul_expr OP_TIMES unary_expr
+    ExpressaoMultiplicacao* res = new ExpressaoMultiplicacao();
+    res->esquerda = extrai_expressao(no->filhos[0]);
+    res->direita = extrai_expressao(no->filhos[2]);
+    return res;
   }
-  case 22: { // 22) E -> E + T
-    ExpressaoSoma* res22 = new ExpressaoSoma();
-    res22->esquerda = extrai_expressao(no->filhos[0]);
-    res22->direita = extrai_expressao(no->filhos[2]);
-    return res22;
+  case 47: { // 47) mul_expr -> mul_expr OP_DIV unary_expr
+    ExpressaoDivisao* res = new ExpressaoDivisao();
+    res->esquerda = extrai_expressao(no->filhos[0]);
+    res->direita = extrai_expressao(no->filhos[2]);
+    return res;
   }
-  case 23: { // 23) E -> E - T
-    ExpressaoSubtracao* res23 = new ExpressaoSubtracao();
-    res23->esquerda = extrai_expressao(no->filhos[0]);
-    res23->direita = extrai_expressao(no->filhos[2]);
-    return res23;
+  case 48: { // 48) mul_expr -> mul_expr OP_MOD unary_expr
+    ExpressaoMod* res = new ExpressaoMod();
+    res->esquerda = extrai_expressao(no->filhos[0]);
+    res->direita = extrai_expressao(no->filhos[2]);
+    return res;
   }
-  case 24: { // 24) E -> T
-    return extrai_expressao(no->filhos[0]);
-  }
-  case 25: { // 25) T -> T * F
-    ExpressaoMultiplicacao* res25 = new ExpressaoMultiplicacao();
-    res25->esquerda = extrai_expressao(no->filhos[0]);
-    res25->direita = extrai_expressao(no->filhos[2]);
-    return res25;
-  }
-  case 26: {// 26) T -> T / F
-    ExpressaoDivisao* res26 = new ExpressaoDivisao();
-    res26->esquerda = extrai_expressao(no->filhos[0]);
-    res26->direita = extrai_expressao(no->filhos[2]);
-    return res26;
-  }
-  case 27: { // 27) T -> T % F
-    ExpressaoMod* res27 = new ExpressaoMod();
-    res27->esquerda = extrai_expressao(no->filhos[0]);
-    res27->direita = extrai_expressao(no->filhos[2]);
-    return res27;
-  }
-  case 28: { // 28) T -> F
-    return extrai_expressao(no->filhos[0]);
-  }
-  case 29: { // 29) F -> ( Ea )
+  case 60: { // 60) primary -> LPAREN expr RPAREN
     return extrai_expressao(no->filhos[1]);
   }
-  case 30: { // 30) F -> ID
-    ExpressaoVariavel* res30 = new ExpressaoVariavel();
-    res30->nome = ID::extrai_ID(no->filhos[0]);
-    return res30;
+  case 59: { // 59) primary -> ID
+    ExpressaoVariavel* res = new ExpressaoVariavel();
+    res->nome = ID::extrai_ID(no->filhos[0]);
+    return res;
   }
-  case 31: // 31) F -> DEC_VAL
-  case 32: // 32) F -> FLOAT_VAL
-  case 33: { // 33) F -> BOOL_VAL
+  case 56: // 56) primary -> INT_LIT
+  case 57: // 57) primary -> REAL_LIT
+  case 58: { // 58) primary -> STRING_LIT
     ExpressaoValor* res = new ExpressaoValor();
     res->valor = ValorLiteral::extrai_valor_literal(no->filhos[0]);
     return res;
   }
+  
+  // Regras de repasse (ex: add_expr -> mul_expr)
+  // 27, 31, 33, 35, 42, 45, 49, 51, 55, 64
+  case 27: case 31: case 33: case 35: case 42: case 45: case 49: case 51: case 55: case 64: {
+    return extrai_expressao(no->filhos[0]);
+  }
+  
   default: {
     return NULL;
   }
