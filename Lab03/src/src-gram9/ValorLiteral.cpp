467ceed9d42b83b07a18416d3d4d8278c91ef722
Roberto Ramos <56175688+rrfeisen@users.noreply.github.com>
2026-05-29 19:22:25 -0300

    Refactor ValorLiteral methods for clarity and efficiency

--- Lab03/src/src-gram9/ValorLiteral.cpp
+++ Lab03/src/src-gram9/ValorLiteral.cpp
@@ -14,41 +14,30 @@ string ValorLiteral::como_string() const {
   if (tipo == NULL) return "";
   stringstream ss;
   switch(tipo->valor) {
-  case Tipo::INT:
-    ss << valor_int;
-    break;
-  case Tipo::FLOAT:
-    ss << valor_float;
-    break;
-  case Tipo::BOOL:
-    ss << (valor_bool ? "true" : "false");
-    break;
+  case Tipo::INT: ss << valor_int; break;
+  case Tipo::FLOAT: ss << valor_float; break;
+  case Tipo::BOOL: ss << (valor_bool ? "true" : "false"); break;
   }
   return ss.str();
-}
-
-bool texto_bool(string texto) {
-  transform(texto.begin(), texto.end(), texto.begin(), ::toupper);
-  return texto == "TRUE" || texto == "1";
 }
 
 ValorLiteral* ValorLiteral::extrai_valor_literal(No_arv_parse* no) {
   if (no == NULL) return NULL;
   ValorLiteral* res = new ValorLiteral();
-  res->tipo = Tipo::extrai_tipo_literal(no);
-  if (res->tipo == NULL) return res;
-
-  switch(res->tipo->valor) {
-  case Tipo::INT:
-    res->valor_int = atoi(no->dado_extra.c_str());
-    break;
-  case Tipo::FLOAT:
-    res->valor_float = atof(no->dado_extra.c_str());
-    break;
-  case Tipo::BOOL:
-    res->valor_bool = texto_bool(no->dado_extra);
-    break;
+  
+  res->tipo = new Tipo(Tipo::INT);
+  if (no->simb == "INT_LIT") {
+      res->valor_int = atoi(no->dado_extra.c_str());
+  } else if (no->simb == "REAL_LIT") {
+      res->tipo->valor = Tipo::FLOAT;
+      res->valor_float = atof(no->dado_extra.c_str());
+  } else if (no->simb == "KW_TRUE") {
+      res->tipo->valor = Tipo::BOOL;
+      res->valor_bool = true;
+  } else if (no->simb == "KW_FALSE") {
+      res->tipo->valor = Tipo::BOOL;
+      res->valor_bool = false;
   }
-
+  
   return res;
 }
