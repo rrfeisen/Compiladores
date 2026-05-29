d9e67d1ed7f45ec1b4d03c189634d5b9d19ecf95
Roberto Ramos <56175688+rrfeisen@users.noreply.github.com>
2026-05-29 14:49:32 -0300

    Add new includes and member variables to Analisador

--- Lab03/src/src-gram9/Analisador.hpp
+++ Lab03/src/src-gram9/Analisador.hpp
@@ -2,12 +2,20 @@
 #define _ANALISADOR_HPP_
 #include "Funcao.hpp"
 #include "ValorLiteral.hpp"
+#include "Expressao.hpp"
+#include "Comando.hpp"
 #include <vector>
+#include <map>
+#include <string>
 using namespace std;
 
 class Analisador {
 public:
+  map<string, ValorLiteral> tabela_simbolos;
+  ValorLiteral ultimo_valor_atribuido;
+
   int calcula_retorno(Funcao *f, const vector<ValorLiteral> &params);
+  void executar_comando(Comando* c);
+  ValorLiteral avaliar_expressao(Expressao* e);
 };
-  
 #endif
