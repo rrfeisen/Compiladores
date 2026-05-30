689c754def3fb07608e3c1bb088a5e91523bf48d
Roberto Ramos <56175688+rrfeisen@users.noreply.github.com>
2026-05-29 21:45:40 -0300

    Delete Lab03_teste directory

--- Lab03_teste/src/debug-util.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef _DEBUG_UTIL_HPP_
-#define _DEBUG_UTIL_HPP_
-void tab3(int tab);
-#endif
--- Lab03_teste/src/src-gram9/ID.cpp
+++
@@ -1,7 +0,0 @@
-#include "ID.hpp"
-
-ID* ID::extrai_ID(No_arv_parse* no) {
-  ID* res = new ID();
-  res->nome = no->dado_extra;
-  return res;
-}
